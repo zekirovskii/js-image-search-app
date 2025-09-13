e221ca36bc362cb18457084157e1a4b2e6968679
YusufSengoz <135019752+zekirovskii@users.noreply.github.com>
2025-09-13 15:31:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: "/", // 👈 Vercel için kök path
     build: {
       sourcemap: true,
       rollupOptions: {
